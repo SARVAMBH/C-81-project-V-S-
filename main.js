9cea270b5a860a8cccd6631f9e49148f1f789afa
SARVAMBH <83635572+SARVAMBH@users.noreply.github.com>
2021-08-08 13:29:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+var canvas=document.getElementById("myCanvas");
+
+var ctx=canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.strokeStyle="red";
+ctx.linewidth=1;
+ctx.rect(150,143,430,200)
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle="blue";
+ctx.linewidth=5;
+ctx.arc(250,210,40,0,2 * Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle="black";
+ctx.linewidth=5;
+ctx.arc(350,210,40,0,2 * Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle="red";
+ctx.linewidth=10;
+ctx.arc(450,210,40,0,2 * Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle="yellow";
+ctx.linewidth=10;
+ctx.arc(300,250,40,0,2 * Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle="darkgreen";
+ctx.linewidth=10;
+ctx.arc(400,250,40,0,2 * Math.PI);
+ctx.stroke();
